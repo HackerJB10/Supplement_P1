920a8d11b96fbb5bbf8bbc12a15a57bc34b7f9ce
Justin Brown-Russell <justinbrownrussell@gmail.com>
2025-01-30 21:38:21 -0500

    Chore: Declared variables of area functions

--- Supplements.Tests/UnitTest1.cs
+++ Supplements.Tests/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace Supplements.Tests;
+﻿namespace Supplements.Tests;
 
 public class UnitTest1
 {
-    
     [Fact]
-    public void ShouldThisCalculateCorrectlyValuesAreaOfRectangle()
+    public void ShouldThisCalculateCorrectly()
     {
-         Assert.True(50.0 == Supplement_P1.AreaOfRectangle(5.0,10.0));
+        Assert.True(50.0 == Supplement_P1.AreaOfRectangle(5.0,10.0));
     }
 }
--- Supplements/Supplement_P1.cs
+++ Supplements/Supplement_P1.cs
@@ -1,18 +1,15 @@
-namespace Supplements;
+﻿namespace Supplements;
 
 public class Supplement_P1
 {
-   
     public static double AreaOfRectangle(double length, double width){
+       return length * width;
+    }
+     public static double AreaOfCircle(double radius){
        throw new NotImplementedException();
     }
 
-     public static double AreaOfCircle(double length, double width){
+ public static double AreaOfTriangle(double Tribase, double TriHeight){
        throw new NotImplementedException();
     }
-
-     public static double AreaOfTriangle(double length, double width){
-       throw new NotImplementedException();
-    }
-
 }
