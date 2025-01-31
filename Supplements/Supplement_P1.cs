e4c3911770bc523bd2240406cc3c5957ad2660d1
Justin Brown-Russell <justinbrownrussell@gmail.com>
2025-01-30 22:04:50 -0500

    Test:Testing for the Area of a Triangle

--- Supplements/Supplement_P1.cs
+++ Supplements/Supplement_P1.cs
@@ -1,20 +1,10 @@
-namespace Supplements;
+﻿namespace Supplements;
 
 public class Supplement_P1
 {
     public static double AreaOfRectangle(double length, double width){
        return length * width;
     }
-
-      
- 
-    }
-/// <summary>
-    /// Returns the radius of a circle 
-    /// </summary>
-    /// <param name="radius">Circle radius</param>
-    /// <returns>The area of a unit circle witha given radius</returns>
-
      public static double AreaOfCircle(double radius){
        throw new NotImplementedException();
     }
