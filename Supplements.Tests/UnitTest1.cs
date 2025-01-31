134219129f8720cf7c10d9e6f9ff4a22570d65f9
Justin Brown-Russell <justinbrownrussell@gmail.com>
2025-01-30 22:00:24 -0500

    feat:implement for the Area of a Circle

--- Supplements.Tests/UnitTest1.cs
+++ Supplements.Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-namespace Supplements.Tests;
+﻿namespace Supplements.Tests;
 
 public class UnitTest1
 {
@@ -7,18 +7,4 @@ public class UnitTest1
     {
         Assert.True(50.0 == Supplement_P1.AreaOfRectangle(5.0,10.0));
     }
-
-    [Fact]
-    public void ShouldThisCalculateCorrectlySameValuesAreaOfRectangles()
-    {
-        Assert.True(25.0 == Supplement_P1.AreaOfRectangle(5.0,5.0));
-    }
-
-    [Fact]
-    public void ShouldThisCalculateCorrectlyValuesAreaOfUnitCircle()
-    {
-        Assert.True(double.Pi == Supplement_P1.AreaOfCircle(1.0));
-    }
-
-
 }
