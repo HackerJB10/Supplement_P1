3e1d651e1010e3991a533bc4d90712fcfee6294e
Justin Brown-Russell <justinbrownrussell@gmail.com>
2025-01-30 21:58:15 -0500

    Test:Testing for the Area of Unit Circle

--- Supplements/Supplement_P1.cs
+++ Supplements/Supplement_P1.cs
@@ -1,15 +1,7 @@
-namespace Supplements;
+﻿namespace Supplements;
 
 public class Supplement_P1
 {
-   /// <summary>
-   /// Returns the area of a rectangle.
-   /// If the length matches the width, the does length * length instead.
-   /// </summary>
-   /// <param name="length">length of the rectangle</param> 
-   /// <param name="width">width of the rectangle</param> 
-   /// <returns> Area of the rectangle</returns>
-
     public static double AreaOfRectangle(double length, double width){
        return length * width;
     }
