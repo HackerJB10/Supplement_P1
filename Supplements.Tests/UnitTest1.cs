343125f6bbed712d617bd49096a505b52c506325
Justin Brown-Russell <justinbrownrussell@gmail.com>
2025-01-30 22:08:11 -0500

    Doc:Documentation for Area of a Triangle

--- Supplements.Tests/UnitTest1.cs
+++ Supplements.Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-namespace Supplements.Tests;
+﻿namespace Supplements.Tests;
 
 public class UnitTest1
 {
@@ -7,25 +7,4 @@ public class UnitTest1
     {
         Assert.True(50.0 == Supplement_P1.AreaOfRectangle(5.0,10.0));
     }
-
-    [Fact]
-    public void ShouldThisCalculateCorrectlySameValuesAreaOfRectangles()
-    {
-        Assert.True(25.0 == Supplement_P1.AreaOfRectangle(5.0,5.0));
-    }
-
-    [Fact]
-    public void ShouldThisCalculateCorrectlyValuesAreaOfUnitCircle()
-    {
-        Assert.True(double.Pi == Supplement_P1.AreaOfCircle(1.0));
-    }
-
-    [Fact]
-    public void ShouldThisCalculateCorrectlyValuesAreaOfTriangle()
-    {
-        Assert.True(100.0 == Supplement_P1.AreaOfTriangle(50.0,4.0));
-    }
-
-
-
 }
